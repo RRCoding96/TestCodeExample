f4b96b2882bfc6c7651cb4dad7de035237f65321
김윤욱 <reznov03048@gmail.com>
2024-07-30 17:42:27 +0900

    test: UserService의 read 메소드 테스트 추가

---
+++ src/test/java/com/example/unittestexample/service/UserServiceTest.java
@@ -0,0 +1,71 @@
+package com.example.unittestexample.service;
+
+import com.example.unittestexample.exception.ResourceNotFoundException;
+import com.example.unittestexample.repository.UserEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource("classpath:test-application.yml")
+@SqlGroup({
+    @Sql(value = "/sql/user-service-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+    @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void getByEmail은_ACTIVE_상태인_유저를_찾아올_수_있다() {
+        // given
+        String email = "reznov03048@gmail.com";
+
+        // when
+        UserEntity result = userService.getByEmail(email);
+
+        // then
+        assertThat(result.getNickname()).isEqualTo("reznov03048");
+    }
+
+    @Test
+    void getByEmail은_PENDING_상태인_유저는_찾아올_수_없다() {
+        // given
+        String email = "reznov03049@gmail.com";
+
+        // when
+        // then
+        assertThatThrownBy(() -> {
+            UserEntity result = userService.getByEmail(email);
+        }).isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @Test
+    void getById는_ACTIVE_상태인_유저를_찾아올_수_있다() {
+        // given
+        // when
+        UserEntity result = userService.getById(1);
+
+        // then
+        assertThat(result.getNickname()).isEqualTo("reznov03048");
+    }
+
+    @Test
+    void getById는_PENDING_상태인_유저는_찾아올_수_없다() {
+        // given
+        // when
+        // then
+        assertThatThrownBy(() -> {
+            UserEntity result = userService.getById(2);
+        }).isInstanceOf(ResourceNotFoundException.class);
+    }
+}
