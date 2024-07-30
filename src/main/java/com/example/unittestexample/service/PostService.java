3ceff365f3a9cd53b084bf1c66c8165adcd9a946
김윤욱 <reznov03048@gmail.com>
2024-07-30 16:00:58 +0900

    service 구현

---
+++ src/main/java/com/example/unittestexample/service/PostService.java
@@ -0,0 +1,40 @@
+package com.example.unittestexample.service;
+
+import com.example.unittestexample.exception.ResourceNotFoundException;
+import com.example.unittestexample.model.dto.PostCreateDto;
+import com.example.unittestexample.model.dto.PostUpdateDto;
+import com.example.unittestexample.repository.PostEntity;
+import com.example.unittestexample.repository.PostRepository;
+import com.example.unittestexample.repository.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Clock;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final UserService userService;
+
+    public PostEntity getPostById(long id) {
+        return postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Posts", id));
+    }
+
+    public PostEntity createPost(PostCreateDto postCreateDto) {
+        UserEntity userEntity = userService.getByIdOrElseThrow(postCreateDto.getWriterId());
+        PostEntity postEntity = new PostEntity();
+        postEntity.setWriter(userEntity);
+        postEntity.setContent(postCreateDto.getContent());
+        postEntity.setCreatedAt(Clock.systemUTC().millis());
+        return postRepository.save(postEntity);
+    }
+
+    public PostEntity updatePost(long id, PostUpdateDto postUpdateDto) {
+        PostEntity postEntity = getPostById(id);
+        postEntity.setContent(postUpdateDto.getContent());
+        postEntity.setModifiedAt(Clock.systemUTC().millis());
+        return postRepository.save(postEntity);
+    }
+}
